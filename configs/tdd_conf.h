0c6386a9783eb0afcab6e8fd54c7a6dfd88f8109
sundhhy <418755805@qq.com>
2018-03-15 19:03:03 +0800

    吧LCD的测试框架程序写好了

--- configs/tdd_conf.h
+++
@@ -1,8 +0,0 @@
-#ifndef __INC_tdd_conf_H_
-#define __INC_tdd_conf_H_
-
-#define TDD_ON			1
-#define TDD_LCD			1
-
-
-#endif
--- project/keil/Objects/unit_test.d
+++
@@ -1,4 +0,0 @@
-.\objects\unit_test.o: ..\..\unit_tests\unit_test.c
-.\objects\unit_test.o: ..\..\..\Atmospheric_collection\configs/tdd_conf.h
-.\objects\unit_test.o: ..\..\unit_tests\unit_test.h
-.\objects\unit_test.o: ..\..\unit_tests\test_lcd.h
